fa51c1be1e69d320c337f439cadc62c40ef00d64
Joseane-Guedes <joseane_23@hotmail.com>
2021-06-12 21:55:07 -0300

    reactproject

---
+++ reactproject/README.md
@@ -0,0 +1,5 @@
+# Meu Primeiro Projeto React do Zero
+
+###  O desafio consiste em criar um projetinho react do zero com direito a componetização, consumo de dados internos e uso de props. 
+
+### Após me deparar com alguns problemas de renderização, resolvi verificar alguns erros no google, Stack Overflow e com a monitora. Assim, os problemas foram resolvidos e refiz o projeto com muita atenção novamente seguindo as orientações da professsora e da documentaçao oficial. 
---
+++ reactproject/src/Services/data.js
@@ -0,0 +1,33 @@
+const music = [
+    {
+        id: 1,
+        name: "In My Feelings",
+        singer: "Lana Del Rey",
+        image: "https://i1.sndcdn.com/artworks-ycLTY302VGylGTM2-guCGIA-t500x500.jpg"
+
+    },
+    {
+        id: 2,
+        name: "Affection ",
+        singer: "Cigarettes After Sex",
+        image: "https://studiosol-a.akamaihd.net/uploadfile/letras/albuns/d/f/2/e/467421445889304.jpg"
+
+    },
+    {
+        id: 3,
+        name: " Taro",
+        singer: "Alt-J",
+        image: "https://monkeybuzz.com.br/wp-content/uploads/2017/05/alt-j-2017-artigo-591f619bddeb7-1024x514.jpg"
+
+    },
+    {
+        id: 4,
+        name: "Ophelia",
+        singer: "The Lumineers",
+        image: "https://folkdaworld.com/wp-content/uploads/2018/07/the-lumineers.jpg"
+
+    },
+
+]
+
+export default music
